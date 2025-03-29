a14a1995fc23c6ea4d349c8a14ae53f392c81afb
aikainfo <ajkentleu@gmail.com>
2025-03-30 04:58:44 +0500

    Update db.py

--- manager/db.py
+++ manager/db.py
@@ -1,22 +1,18 @@
 import psycopg2
-
+#this class is responsible for Database
 
 class Database:
+
     def __init__(self, host, user, password, dbName):
-        self.host = host  # Host address where the database is hosted (e.g., localhost or IP address)
-        self.user = user  # Username for connecting to the database
-        self.password = password  # Password for the provided username
-        self.dbName = dbName  # Name of the database to connect to
+        self.host = host
+        self.user = user
+        self.password = password
+        self.dbName = dbName
 
     def connection(self):
         try:
-            conn = psycopg2.connect(  # Attempt to connect to the database using the provided credentials
-                dbname=self.dbName,
-                user=self.user,
-                password=self.password,
-                host=self.host
-            )
-            print('Connection established')  # Print success message if connection is successful
-            return conn  # Return the connection object for further usage
+            conn = psycopg2.connect(dbname=self.dbName, user=self.user, password=self.password, host=self.host)
+            print('Connection established')
+            return conn
         except:
-            print('Can`t establish connection to database')  # Print an error message if the connection attempt fails
+            print('Can`t establish connection to database')
